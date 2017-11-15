c7d569ee970df3297e66528ff3256d0ee9f55ae9
Karim Vaes <kvaes@github.com>
2017-11-15 14:24:15 +0100

    Disk config form

--- application/views/vmchooser-form-disk.php
+++ application/views/vmchooser-form-disk.php
@@ -60,16 +60,14 @@ echo form_open(base_url(), $attributes);
 <fieldset>
   <div class="form-group">
     <div class="col-lg-10 col-lg-offset-2">
-	  <button type="submit" class="btn btn-primary">Let's try to match a VM t-shirt size for you!</button>
+	  <button type="submit" class="btn btn-primary">Let's try to match a disk configuration for you!</button>
     </div>
   </div>
 </fieldset>
 
 <ul class="nav nav-tabs">
   <li class="active"><a href="#basic" data-toggle="tab" aria-expanded="true">Requirements</a></li>
-  <li class=""><a href="#advanced" data-toggle="tab" aria-expanded="false">Advanced</a></li>
-  <li class=""><a href="#rightsizing" data-toggle="tab" aria-expanded="false">Rightsizing</a></li>
-	<li class=""><a href="#options" data-toggle="tab" aria-expanded="false">Options</a></li>
+  <li class=""><a href="#options" data-toggle="tab" aria-expanded="false">Options</a></li>
 </ul>
 <div id="myTabContent" class="tab-content">
   <div class="tab-pane fade active in" id="basic">
@@ -144,134 +142,29 @@ echo form_open(base_url(), $attributes);
 		  </div>
 		</div>
 		<div class="form-group">
-		  <label for="inputCores" class="col-lg-2 control-label">Number of Cores</label>
+		  <label for="inputData" class="col-lg-2 control-label">Capacity</label>
 		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputCores" id="inputCores"  value="<?php echo set_value('inputCores[]'); ?>" placeholder="What's the minimum of cores this VM needs?" autocomplete="off">
+			<input type="text" class="form-control" name="inputData" id="inputData" value="<?php echo set_value('inputData[]'); ?>" placeholder="What's the minimum capacity / size (in GB) for this disk config?" autocomplete="off">
 		  </div>
 		</div>
-		<div class="form-group">
-		  <label for="inputMemory" class="col-lg-2 control-label">Amount of Memory</label>
-		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputMemory" id="inputMemory" value="<?php echo set_value('inputMemory[]'); ?>" placeholder="What's the minimum amount of memory (in GB) this VM needs?" autocomplete="off">
-		  </div>
-		</div>
-		<div class="form-group">
-		  <label for="inputNics" class="col-lg-2 control-label">Number of NICs</label>
-		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputNics" id="inputNics" value="<?php echo set_value('inputNics[]'); ?>" placeholder="What's the minimum number of network interfaces this this VM needs?" autocomplete="off">
-		  </div>
-		</div>
-		<div class="form-group">
-		  <label for="inputData" class="col-lg-2 control-label">Minimum disk size</label>
-		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputData" id="inputData" value="<?php echo set_value('inputData[]'); ?>" placeholder="What's the minimum disk size (in TB) needed? (excluding the OS disk)" autocomplete="off">
-		  </div>
-		</div>
-	 </fieldset>
-    
-  </div>
-  <div class="tab-pane fade" id="advanced">
-  
-	<fieldset>
-		<legend>Advanced requirements for your virtual machine</legend>
-		<div class="form-group">
-		  <label class="col-lg-2 control-label">Tier</label>
-		  <div class="col-lg-10">
-			<div class="radio">
-			  <label>
-				<input type="radio" name="inputTier" id="optionsRadios1" value="standard" <?php echo  set_radio('inputTier', 'standard', TRUE); ?>>
-				Standard
-			  </label>
-			</div>
-			<div class="radio">
-			  <label>
-				<input type="radio" name="inputTier" id="optionsRadios2" value="lowpriority" <?php echo  set_radio('inputTier', 'lowpriority', FALSE); ?>>
-				Low Priority
-			  </label>
-			</div>
-			<div class="radio">
-			  <label>
-				<input type="radio" name="inputTier" id="optionsRadios2" value="basic" <?php echo  set_radio('inputTier', 'basic', FALSE); ?>>
-				Basic
-			  </label>
-			</div>
-		  </div>
-		</div>
-		<div class="form-group">
-		  <label class="col-lg-2 control-label">Hyperthreaded Core</label>
-		  <div class="col-lg-10">
-			<div class="radio">
-			  <label>
-				<input type="radio" name="inputHt" id="optionsRadios1" value="Yes" <?php echo  set_radio('inputHt', 'Yes', FALSE); ?>>
-				Yes
-			  </label>
-			</div>
-			<div class="radio">
-			  <label>
-				<input type="radio" name="inputHt" id="optionsRadios2" value="No" <?php echo  set_radio('inputHt', 'No', FALSE); ?>>
-				No
-			  </label>
-			</div>
-			<div class="radio">
-			  <label>
-				<input type="radio" name="inputHt" id="optionsRadios2" value="All" <?php echo  set_radio('inputHt', 'All', TRUE); ?>>
-				Don't care... 
-			  </label>
-			</div>
-		  </div>
-		</div>
-		<div class="form-group">
-		  <label for="inputPcores" class="col-lg-2 control-label">Number of "pCores"</label>
-		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputPcores" id="inputPcores"  value="<?php echo set_value('inputPcores[]'); ?>" placeholder="What's the minimum equivalent of physical cores this VM needs?" autocomplete="off">
-		  </div>
-		</div>
-		<div class="form-group">
-		  <label for="inputAcu" class="col-lg-2 control-label">ACU</label>
-		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputAcu" id="inputAcu" value="<?php echo set_value('inputAcu[]'); ?>" placeholder="What's the minimum ACU (azure compute unit) you expect for this VM?" autocomplete="off">
-		  </div>
-		</div>
-		<div class="form-group">
+	 
+	 <div class="form-group">
 		  <label for="inputIops" class="col-lg-2 control-label">IOPS</label>
 		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputIops" id="inputIops" value="<?php echo set_value('inputIops[]'); ?>" placeholder="What's the minimum IOPS, for the non-OS disk(s), this VM needs?" autocomplete="off">
+			<input type="text" class="form-control" name="inputIops" id="inputIops" value="<?php echo set_value('inputIops[]'); ?>" placeholder="What's the minimum IOPS for this disk config?" autocomplete="off">
 		  </div>
 		</div>
 		<div class="form-group">
 		  <label for="inputThroughput" class="col-lg-2 control-label">Throughput</label>
 		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputThroughput" id="inputThroughput" value="<?php echo set_value('inputThroughput[]'); ?>" placeholder="What's the minimum throughput (in MB), for the non-OS disk(s), this VM needs?" autocomplete="off">
+			<input type="text" class="form-control" name="inputThroughput" id="inputThroughput" value="<?php echo set_value('inputThroughput[]'); ?>" placeholder="What's the minimum throughput (in MB) for this disk config?" autocomplete="off">
 		  </div>
-		</div>
-		<div class="form-group">
-		  <label for="inputTemp" class="col-lg-2 control-label">Minimum temp disk size</label>
-		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputTemp" id="inputTemp" value="<?php echo set_value('inputTemp[]'); ?>" placeholder="What's the minimum size (in GB) for the temp disk?" autocomplete="off">
-		  </div>
-		</div>
-	 </fieldset>
-    
+		</div>    
   </div>
-  <div class="tab-pane fade" id="rightsizing">
-
-	<fieldset>
-		<legend>Rightsizing your virtual machine</legend>
-		<div class="form-group">
-		  <label for="inputAvgcpupeak" class="col-lg-2 control-label">Peak CPU Usage</label>
-		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputAvgcpupeak" id="inputAvgcpupeak" value="<?php echo set_value('inputAvgcpupeak[]'); ?>" placeholder="What's the peak CPU usage (in %) when checking the metrics of your VM on a 95pct curve?" autocomplete="off">
-		  </div>
-		</div>
-		<div class="form-group">
-		  <label for="inputAvgmempeak" class="col-lg-2 control-label">Peak Memory Usage</label>
-		  <div class="col-lg-10">
-			<input type="text" class="form-control" name="inputAvgmempeak" id="inputAvgmempeak" value="<?php echo set_value('inputAvgmempeak[]'); ?>" placeholder="What's the peak Memory usage (in %) when checking the metrics of your VM on a 95pct curve?" autocomplete="off">
-		  </div>
-		</div>
-	 </fieldset>
-
-	 </div>
+  
+	</fieldset>
+	
+	</div>
 
 	<div class="tab-pane fade" id="options">
 
